25ea3ed23c80ce9851a41c057e4b9b8d9dc9b7b6
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-04-29 21:08:14 -0300

    implementando sistema de disponibilidade de livro

--- app/criarlivros.js
+++ app/criarlivros.js
@@ -4,9 +4,12 @@ function exibirLivros(listaDeLivros){
 
         livrosContainer.innerHTML = '';
     listaDeLivros.forEach( livro => {
+
+        let disponibilidade = verificaDisponibilidade(livro);
+
         livrosContainer.innerHTML += `
         <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+            <img class="livro__imagens ${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
             <h2 class="livro__titulo">${livro.titulo}</h2>
             <p class="livro__descricao">${livro.autor}</p>
             <p class="livro__preco" id="preco">R$ ${livro.preco.toFixed(2)}</p>
@@ -15,10 +18,13 @@ function exibirLivros(listaDeLivros){
             <p hidden>${livro.quantidade}</p>
             </div>
         </div>        `
-        if(livro.quantidade <= 0){
-            let img = document.querySelector('.livro__imagens');
-            img.classList.add('indisponivel');
-        }
-    });
+})
+}
 
+function verificaDisponibilidade(livro){
+    if( livro.quantidade > 0){
+       return 'livro__imagens';
+    }else{
+        return 'indisponivel';
+    }
 }
